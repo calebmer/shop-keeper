c68d4d8a017bdc9aafd17bf05fcdf5cfd80a6356
calebmer <calebmeredith8@gmail.com>
2015-07-30 11:42:47 -0400

    Fix wierd delete hook interactions

--- src/endpoint/record.js
+++ src/endpoint/record.js
@@ -139,16 +139,12 @@ class RecordEndpoint extends Endpoint {
     Async.waterfall([
       done => this.ensureAccountability(req, done),
 
-      done => this.collection.executeHook('delete', { id: recordId }, done),
-
       done =>
         table
         .select(table.star())
         .where(table.id.equals(recordId))
         .limit(1)
         ::exec(done),
-        // TODO: consider not selecting this document and only sending
-        // { id: recordId } to the hook
       ([record], done) => this.collection.executeHook('delete', record, done),
 
       done =>
